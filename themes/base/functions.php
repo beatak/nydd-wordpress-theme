94cbe265463acf2c9ecb36544150f6dfa1c5a3fa
Takashi Mizohata <beatak@gmail.com>
2011-12-28 18:32:06 -0500

    no translation and no random image

--- themes/base/functions.php
+++ themes/base/functions.php
@@ -72,19 +72,6 @@ if ( ! function_exists( 'twentyeleven_setup' ) ):
  * @since Twenty Eleven 1.0
  */
 function twentyeleven_setup() {
-
-	/* Make Twenty Eleven available for translation.
-	 * Translations can be added to the /languages/ directory.
-	 * If you're building a theme based on Twenty Eleven, use a find and replace
-	 * to change 'twentyeleven' to the name of your theme in all the template files.
-	 */
-	load_theme_textdomain( 'twentyeleven', get_template_directory() . '/languages' );
-
-	$locale = get_locale();
-	$locale_file = get_template_directory() . "/languages/$locale.php";
-	if ( is_readable( $locale_file ) )
-		require_once( $locale_file );
-
 	// This theme styles the visual editor with editor-style.css to match the theme style.
 	add_editor_style();
 
@@ -115,12 +102,14 @@ function twentyeleven_setup() {
 	define( 'HEADER_TEXTCOLOR', '000' );
 
 	// By leaving empty, we allow for random image rotation.
-	define( 'HEADER_IMAGE', '' );
+	// define( 'HEADER_IMAGE', '' );
 
 	// The height and width of your custom header.
 	// Add a filter to twentyeleven_header_image_width and twentyeleven_header_image_height to change these values.
-	define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyeleven_header_image_width', 1000 ) );
-	define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyeleven_header_image_height', 288 ) );
+	// define( 'HEADER_IMAGE_WIDTH', apply_filters( 'twentyeleven_header_image_width', 1000 ) );
+	// define( 'HEADER_IMAGE_HEIGHT', apply_filters( 'twentyeleven_header_image_height', 288 ) );
+  define( 'HEADER_IMAGE_WIDTH', '' );
+  define( 'HEADER_IMAGE_HEIGHT', '' );
 
 	// We'll be using post thumbnails for custom header images on posts and pages.
 	// We want them to be the size of the header image that we just defined
@@ -132,11 +121,11 @@ function twentyeleven_setup() {
 	add_image_size( 'small-feature', 500, 300 ); // Used for featured posts if a large-feature doesn't exist
 
 	// Turn on random header image rotation by default.
-	add_theme_support( 'custom-header', array( 'random-default' => true ) );
+	// add_theme_support( 'custom-header', array( 'random-default' => true ) );
 
 	// Add a way for the custom header to be styled in the admin panel that controls
 	// custom headers. See twentyeleven_admin_header_style(), below.
-	add_custom_image_header( 'twentyeleven_header_style', 'twentyeleven_admin_header_style', 'twentyeleven_admin_header_image' );
+	// add_custom_image_header( 'twentyeleven_header_style', 'twentyeleven_admin_header_style', 'twentyeleven_admin_header_image' );
 
 	// ... and thus ends the changeable header business.
 
