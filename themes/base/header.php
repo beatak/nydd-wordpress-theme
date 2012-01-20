5b25079cef3ada0f85c7cdcee6defeb2e96c0d46
Takashi Mizohata <beatak@gmail.com>
2012-01-19 22:23:52 -0500

    wip: structual changing

--- themes/base/header.php
+++ themes/base/header.php
@@ -70,7 +70,7 @@
 <body <?php body_class(); ?>>
 
 	<header id="branding" role="banner">
-			<hgroup>
+			<hgroup class="clearfix">
 				<h1 id="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 				<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
 			</hgroup>
