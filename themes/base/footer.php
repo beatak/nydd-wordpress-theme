632479a58fc824e2c3359a0ca8e6e3b2cd7600ef
Takashi Mizohata <beatak@gmail.com>
2012-02-23 12:13:52 +0900

    footer structure change

--- themes/base/footer.php
+++ themes/base/footer.php
@@ -11,23 +11,21 @@
 ?>
 
 	</div><!-- #main -->
-
-	<footer id="colophon" role="contentinfo">
-
-			<?php
-				/* A sidebar in the footer? Yep. You can can customize
-				 * your footer with three columns of widgets.
-				 */
-				if ( ! is_404() )
-					get_sidebar( 'footer' );
-			?>
-
-			<div id="site-generator">
-				<?php do_action( 'twentyeleven_credits' ); ?>
-				<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
-			</div>
-	</footer><!-- #colophon -->
 </div><!-- #page -->
+
+<footer id="colophon" role="contentinfo">
+  <?php
+    /* A sidebar in the footer? Yep. You can can customize
+     * your footer with three columns of widgets.
+     */
+    if ( ! is_404() )
+      get_sidebar( 'footer' );
+  ?>
+	<div id="site-generator">
+    <?php do_action( 'twentyeleven_credits' ); ?>
+    <a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentyeleven' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentyeleven' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'twentyeleven' ), 'WordPress' ); ?></a>
+  </div>
+</footer><!-- #colophon -->
 
 <?php wp_footer(); ?>
 
